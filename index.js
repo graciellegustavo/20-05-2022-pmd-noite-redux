475e7c527150a0736590624f1ec52782e3e08b68
unknown <aluno@L4M08.IPIRANGA.FATEC>
2022-05-20 21:43:47 -0300

    fim da secao 2.10

--- index.js
+++ index.js
@@ -30,3 +30,48 @@ const solicitarCashback = (nome, valor) => {
         payload: { nome, valor }
     }
 }
+
+//arrow function
+//historicoDePedidosCashbackReducer
+//recebe uma fatia de estado sobre a qual operar (uma lista chamada historicoDePedidosCashback. Por padrão, ela é uma lista vazia)
+//recebe uma ação
+//devolve: uma lista que contém todos os pedidos da lista recebida e o pedido descrito no payload da ação 
+//detalhe: somente operar no estado se o type for apropriado
+//detalhe2: obgatório usar o spread (...)
+
+const historicoDePedidosCashbackReducer = (historicoDePedidosCashback = [], acao) => {
+    if (acao.type === "SOLICITAR_CASHBACK") {
+        return [...historicoDePedidosCashback, acao.payload]
+    }
+    return historicoDePedidosCashback
+}
+
+//implementar o reducer que manipula o caixa 
+//o caixa começa zerado
+
+const caixaReducer = (valorDeCaixa = 0, acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return valorDeCaixa + acao.payload.valor
+    }
+
+    if (acao.type === "SOLICITAR_CASHBACK") {
+        return valorDeCaixa - acao.payload.valor
+    }
+
+    return valorDeCaixa
+}
+
+//implementar o reducer que lida com a lista de contratos
+//elepode criar contrato e cancelar contratos
+
+const listaReducer = (contratos = [], acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return [...contratos, acao.payload]
+    }
+
+    if (acao.type === "CANCELAR_CONTRATO") {
+        return contratos.filter(contrato => contrato.nome !== acao.payload.nome )
+    }
+
+    return contratos
+}
